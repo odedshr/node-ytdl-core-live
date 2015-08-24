45a7adb08e2ff68a11c85bf641e6b5bec2247e5d
Oded Sharon <odedsh@gmail.com>
2015-08-24 23:43:58 +0100

    basic working version

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    url = require('url'),
+    fs = require('fs'),
+    ytdl = require('ytdl-core');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+    var url_parts = url.parse(request.url, true),
+        query = url_parts.query,
+        youtubeURL = decodeURI(query["url"]);
+
+    if (typeof youtubeURL!== "undefined") {
+        response.setHeader('Content-disposition', 'attachment; filename='+url.parse(youtubeURL, true).query["v"]+'.mp3');
+        response.setHeader('Content-type', 'audio/mp3');
+        ytdl(youtubeURL, {filter: "audioonly"}).pipe(response);
+        //response.send("saved?");
+    } else {
+        response.send("URL is missing");
+    }
 });
 
 app.listen(app.get('port'), function() {
