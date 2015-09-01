c06fb79ac70a80df170f97ac21cd04f2d8be6da0
Oded Sharon <odedsh@gmail.com>
2015-09-01 23:45:19 +0100

    added ping

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ app.get('/', function(request, response) {
     }
 });
 
+app.get('/ping', function(request, response) {
+    response.end("pong");
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
